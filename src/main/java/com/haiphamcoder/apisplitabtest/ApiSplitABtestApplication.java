7b0c2979ed2fa214a285741faa6de5df76c0aa4c
Hai Pham <ngochai285nd@gmail.com>
2023-11-29 10:00:42 +0700

    init project

---
+++ src/main/java/com/haiphamcoder/apisplitabtest/ApiSplitABtestApplication.java
@@ -0,0 +1,13 @@
+package com.haiphamcoder.apisplitabtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSplitABtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSplitABtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haiphamcoder/apisplitabtest/ApiSplitABtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiphamcoder.apisplitabtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSplitABtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
